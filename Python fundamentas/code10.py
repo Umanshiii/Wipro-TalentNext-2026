8dfd18d41313abcaf255f47791db72fe031f5b5f
Umanshi <umanshi121@gmail.com>
2026-06-27 01:49:21 +0530

    Check if a number is palindrome

--- Python fundamentas/code10.py
+++ Python fundamentas/code10.py
@@ -10,10 +10,15 @@ O/P: 1234 is not a palindrome.
 '''
 
 def palindrome(num):
-    reverse=''
-    for i in range(len(str(num))-1,-1,-1):
-        reverse+=str(num)[i]
-    if num==int(reverse):
+    temp = num
+    reverse = 0
+
+    while temp > 0:
+        digit = temp % 10
+        reverse = reverse * 10 + digit
+        temp //= 10
+
+    if num == reverse:
         return f"{num} is a palindrome"
     else:
         return f"{num} is not a palindrome"
