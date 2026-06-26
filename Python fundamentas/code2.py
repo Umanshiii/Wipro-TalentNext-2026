69544e12a8a59a6c6a2977afa98ee867558f0493
Umanshi <umanshi121@gmail.com>
2026-06-27 00:10:37 +0530

    basic if else

---
+++ Python fundamentas/code2.py
@@ -0,0 +1,9 @@
+#Write a program to check if a number is odd or even
+
+num=int(input("Enter a number: "))
+
+if num%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+    
