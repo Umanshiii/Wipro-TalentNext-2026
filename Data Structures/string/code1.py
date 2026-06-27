87d43e1341d212787bcf84407a4885a8f6be596f
Umanshi <umanshi121@gmail.com>
2026-06-28 03:04:43 +0530

    Count upper and lowercase letters

--- Data Structures/string/code1.py
+++ Data Structures/string/code1.py
@@ -1,16 +1,13 @@
 # Write a program to count the number of upper and lower case letters in a String.
 
 s='ThisIsTheString'
-def count_letters(s):
-    upper=0
-    lower=0
+upper=0
+lower=0
 
-    for i in s:
-        if 65<=ord(i)<=90:
-            upper+=1
-        elif 97<=ord(i)<=122:
-            lower+=1
+for i in s:
+    if 65<=ord(i)<=90:
+        upper+=1
+    elif 97<=ord(i)<=122:
+        lower+=1
 
-    return f"Upper case character count = {upper} and Lower case character count = {lower}"
-
-print(count_letters(s))
+print(f"Upper case character count = {upper} and Lower case character count = {lower}")
