cdbfbe71e2d9a7d47007aab9e2b7ca4667eaaec8
Umanshi <umanshi121@gmail.com>
2026-06-27 18:39:05 +0530

    create and display list

---
+++ Data Structures/list/code1.py
@@ -0,0 +1,5 @@
+#Write a program to create a list of 5 integers and display the list items. Access individual elements through index.
+
+lis= [1,2,3,4,5]
+for i in range(len(lis)):
+    print(lis[i])
