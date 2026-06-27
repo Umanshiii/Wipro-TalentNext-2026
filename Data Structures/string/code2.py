5da05ab273404a39f952109b863a99eb78b764fa
Umanshi <umanshi121@gmail.com>
2026-06-28 03:05:34 +0530

    Paindrome string

--- Data Structures/string/code2.py
+++ Data Structures/string/code2.py
@@ -1,20 +1,18 @@
 # Write a program that will check whether a given String is Palindrome or not.
 
-def palindrome(s):
-    s = s.lower()
-    left=0
-    right=len(s)-1
-
-    while left<right:
-
-        if s[left] != s[right]:
-            return "The given string is not a palindrome"
-        left+=1
-        right-=1
-
-    
-    else:
-        return "The given string is palindrome"
-
 s='You Do Do You'
-print(palindrome(s))
+s = s.lower()
+left=0
+right=len(s)-1
+
+while left<right:
+
+    if s[left] != s[right]:
+        print("The given string is not a palindrome")
+        break
+    left+=1
+    right-=1
+  
+else:
+    print("The given string is palindrome")
+
