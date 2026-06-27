5efafa8140c3dde29acd7b91effce05d3fe10db0
Umanshi <umanshi121@gmail.com>
2026-06-27 19:17:16 +0530

    Create a dictionary

---
+++ Data Structures/dictionary/code5.py
@@ -0,0 +1,7 @@
+# Write a program to prepare a dictionary where the keys are numbers between 1 and 15 (both included) and the values are square of the keys.
+
+dic={}
+for i in range(1,16):
+    dic[i]=i**2
+
+print(dic)
