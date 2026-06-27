b56bbed9c4bbea30b4a0383a28e022e8279adc25
Umanshi <umanshi121@gmail.com>
2026-06-27 20:21:58 +0530

    replace last item of tuple in list

---
+++ Data Structures/tuple/code5.py
@@ -0,0 +1,19 @@
+# Write a program to replace last value of tuples in a list to 100.  
+'''
+Sample list: [(10, 20, 40), (40, 50, 60), (70, 80, 90)] 
+Expected Output: [(10, 20, 100), (40, 50, 100), (70, 80, 100)]
+'''
+
+tup=[(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+
+def replace(tup):
+
+    for i in range(len(tup)):
+
+        lis=list(tup[i])
+        lis[-1]=100
+        tup[i]=tuple(lis)
+
+    return tup
+
+print(replace(tup))
