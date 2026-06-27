50806ec683efaafdf1c8a87b9395593a272ff11a
Umanshi <umanshi121@gmail.com>
2026-06-28 02:44:34 +0530

    check if number is positive, negative or zero

--- Python fundamentas/code3.py
+++ Python fundamentas/code3.py
@@ -4,14 +4,11 @@ lastDigit(7, 17) → true
 lastDigit(6, 17) → false
 lastDigit(3, 113) → true
 '''
+input1=input("Enter the 1st number: ")   
+input2=input("Enter the 2nd number: ")
 
-def lastdigit(num1, num2):
-    input1=str(num1)
-    input2=str(num2)
+if input1[-1]==input2[-1]:
+    print(True)
+else:
+    print(False)
 
-    if input1[-1]==input2[-1]:
-        return True
-    else:
-        return False
-
-lastdigit(56,78)
