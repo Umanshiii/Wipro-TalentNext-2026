b644a21670031860038e9196cc21be2dd03a201a
Umanshi <umanshi121@gmail.com>
2026-06-28 15:28:20 +0530

    set operations

--- Data Structures/set/code2.py
+++ Data Structures/set/code2.py
@@ -1,19 +1,18 @@
 # Write a program to create an intersection of sets.
 
-def intersection(set1, set2):
-    if len(set1)<len(set2):
-        small=set1
-        large=set2
-    else:
-        small=set2
-        large=set1
-    result=set()
-    for i in small:
-        if i in large:
-            result.add(i)
-
-    return result
-
 set1={2,3,4,6,67}
 set2={3,4,6,77,2}
-print(intersection(set1,set2))
+if len(set1)<len(set2):
+    small=set1
+    large=set2
+else:
+    small=set2
+    large=set1
+result=set()
+for i in small:
+    if i in large:
+        result.add(i)
+
+print(result)
+
+
--- Data Structures/set/code3.py
+++ Data Structures/set/code3.py
@@ -1,25 +1,22 @@
 # Write a program to create an union of sets.
 
-def union(set1, set2):
-    if len(set1)<len(set2):
-        small=set1
-        large=set2
-    else:
-        small=set2
-        large=set1
-    result=set()
-    for i in large:
-        if i in result:
-            continue
-        result.add(i)
-        
-    for i in small:
-        if i in result:
-            continue
-        result.add(i)
-
-    return result
-
 set1={2,3,4,6,67,8}
 set2={3,4,6,77,24,5}
-print(union(set1,set2))
+if len(set1)<len(set2):
+    small=set1
+    large=set2
+else:
+    small=set2
+    large=set1
+result=set()
+for i in large:
+    if i in result:
+        continue
+    result.add(i)
+        
+for i in small:
+    if i in result:
+        continue
+    result.add(i)
+
+print(result)
